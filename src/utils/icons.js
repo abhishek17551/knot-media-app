f8a9b6ccc781352c1cad9f9b7e0007b2ff1ae685
abhishek17551 <ababhishek113@gmail.com>
2023-07-04 09:34:29 +0530

    feat;added icons

---
+++ src/utils/icons.js
@@ -0,0 +1,80 @@
+import {
+    FaHeart,
+    FaEdit,
+    FaTrash,
+    FaUserPlus,
+    FaArrowLeft,
+    FaTimes,
+    FaCamera,
+    FaRegBookmark,
+    FaBookmark,
+    FaFilter,
+    FaFire,
+    FaArrowUp,
+    FaArrowDown,
+    FaRegHeart,
+    FaRegComments,
+    FaAngleDoubleLeft,
+}   from 'react-icons/fa'
+
+import {
+    MdPerson,
+    MdDateRange,
+    MdCancel,
+    MdShare,
+    MdOutlineLightMode,
+    MdOutlineDarkMode,
+    MdHome,
+    MdExplore,
+    MdLogout,
+    MdOutlineAddPhotoAlternate,
+    MdInsertEmoticon,
+    MdBookmarks,
+
+} from 'react-icons/md'
+
+import {BsPersonCircle} from 'react-icons/bs'
+
+import {AiOutlineEyeInvisible,AiOutlineEye} from 'react-icons/ai'
+
+import {FiSearch} from 'react-icons/fi'
+
+import {RiUserUnfollowFill} from 'react-icons/ri'
+
+import { HiDotsHorizontal, HiPlusCircle, HiLink} from 'react-icons/hi'
+
+export {
+    HiDotsHorizontal, HiPlusCircle, HiLink,
+    RiUserUnfollowFill,
+    FiSearch,
+    AiOutlineEyeInvisible,AiOutlineEye,
+    BsPersonCircle,
+    MdPerson,
+    MdDateRange,
+    MdCancel,
+    MdShare,
+    MdOutlineLightMode,
+    MdOutlineDarkMode,
+    MdHome,
+    MdExplore,
+    MdLogout,
+    MdOutlineAddPhotoAlternate,
+    MdInsertEmoticon,
+    MdBookmarks,
+    FaHeart,
+    FaEdit,
+    FaTrash,
+    FaUserPlus,
+    FaArrowLeft,
+    FaTimes,
+    FaCamera,
+    FaRegBookmark,
+    FaBookmark,
+    FaFilter,
+    FaFire,
+    FaArrowUp,
+    FaArrowDown,
+    FaRegHeart,
+    FaRegComments,
+    FaAngleDoubleLeft,
+}
