2e34cb272d3e19bc0b45e6df0152f07923819dd4
abhishek17551 <ababhishek113@gmail.com>
2023-07-04 20:17:29 +0530

    feat:created service & reducer for users

---
+++ src/services/usersService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const getAllUsersService = async () => await axios.get('/api/users');
+
+const getUserByUsernameService = async (username) => await axios.get(`/api/users/${username}`);
+
+const getBookmarksService = async (encodedToken) => 
+    await axios.get('/api/users/bookmark', {
+        headers : {authorization : encodedToken}
+    });
+
+const addBookmarkService = async (postId,encodedToken) => 
+    await axios.post(`/api/users/bookmark/${postId}`, {}, {
+        headers : {authorization : encodedToken}
+    });
+
+const removeBookmarkService = async (postId,encodedToken) => 
+    await axios.post(`/api/users/remove-bookmark/${postId}`, {}, {
+        headers : {authorization : encodedToken}
+    });
+
+const followUserService = async (followUserId, encodedToken) => 
+    await axios.post(`/api/users/follow/${followUserId}`, {}, {
+        headers: { authorization: encodedToken }
+    });
+
+const unfollowService = async (followUserId, encodedToken) =>
+    await axios.post(`/api/users/unfollow/${followUserId}`, {}, {
+        headers: { authorization: encodedToken } 
+    });
+
+const editUserProfile = async (editInput, encodedToken) => 
+    await axios.post('/api/users/edit', {
+        userData : editInput
+    }, {
+        headers: { authorization: encodedToken }
+    });
+
+export {
+    editUserProfile,unfollowService,followUserService,
+    removeBookmarkService,addBookmarkService,getBookmarksService,
+    getUserByUsernameService,getAllUsersService
+}
