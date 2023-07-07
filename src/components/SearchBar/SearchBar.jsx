8a9cbfdf0103def14f102f048cbb63d751571141
abhishek17551 <ababhishek113@gmail.com>
2023-07-07 17:58:39 +0530

    styles:added styles to searchbar,searchedusermodal

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,20 +8,21 @@ const SearchBar = () => {
     const {SEARCH_USER} = actionTypes;
 
     return (
-        <div>
-            <div>
+        <div className="sticky top-[15px] z-50 mr-1 xl:mr-0">
+            <div className="relative border border-primaryDark rounded-lg bg-primaryLight dark:bg-primaryDark focus-within:border-secondaryDark xl:mx-4 2xl:my-3">
                 <input 
                     type="text"
                     value={searchInput}
                     placeholder="Search Users..."
                     onChange={(e) => userDispatch({type:SEARCH_USER, payload:e.target.value})}
+                    className="bg-primaryLight dark:bg-primaryDark dark:text-primaryLight outline-none rounded-lg text-sm text-primaryDark py-2 px-4 w-[90%] xl:text-base"
                 />
-                <FiSearch/>
+                <FiSearch className="absolute right-4 top-2.5 xl:top-3.25"/>
             </div>
 
             {
                 searchInput && (
-                    <div>
+                    <div className="absolute top-15 w-[90%] mx-4">
                        <SearchedUsersModal/>
                     </div>
                 )
@@ -29,3 +30,5 @@ const SearchBar = () => {
         </div>
     )
 }
+
+export {SearchBar}
--- src/components/SearchedUsersModal/SearchedUsersModal.jsx
+++ src/components/SearchedUsersModal/SearchedUsersModal.jsx
@@ -9,7 +9,7 @@ const SearchedUsersModal = () => {
     const {SEARCH_USER} = actionTypes;
 
     return (
-        <div>
+        <div className="flex flex-col gap-4 text-sm xl:text-base w-full p-4 z-40 sticky top-0 mt-2">
             {
                 searchedUsers.length ? (
                     searchedUsers.map((user) => (
@@ -19,16 +19,17 @@ const SearchedUsersModal = () => {
                                 navigate(`/profile/${user?.username}`)
                                 userDispatch({type : SEARCH_USER, payload : ""})
                             }}
+                            className="flex flex-start gap-2 cursor-pointer"
                         >
                             <UserAvatar user={user} className="h-8 w-8"  />
-                            <div>
-                                <span>{user.firstName + " " + user.lastName}</span>
-                                <span>@{user.username}</span>
+                            <div className="flex flex-col grow ">
+                                <span className="text-sm -mt-0.5">{user.firstName + " " + user.lastName}</span>
+                                <span className="text-sm text-[grey] -mt-1">@{user.username}</span>
                             </div>
                         </div>
                     ))
                 ) : (
-                    <div>Ooops! No match found for the entered User.</div>
+                    <div className="text-center">Ooops! No match found for the entered User.</div>
                 )
             }
         </div>
