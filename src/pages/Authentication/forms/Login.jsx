2c0631a8d7f22d3f3a1773ea1a523569dfca4c63
abhishek17551 <ababhishek113@gmail.com>
2023-07-07 23:07:06 +0530

    feat:created basic signup.login pages

--- src/pages/Authentication/forms/Login.jsx
+++ src/pages/Authentication/forms/Login.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../../contexts/authContext';
+import { PrimaryButton, SecondaryButton } from '../../../components/Button/Button';
+import { guestUserLoginDetails } from '../../../utils/constants';
 
 function Login() {
+  const navigate = useNavigate();
+  const {loginHandler} = useAuth();
+
+  const [loginDetails,setLoginDetails] = useState({
+    username: "",
+    password: "",
+    showPwd: false,
+  })
+
+  document.title = "Login | Knot"
+
+  const loginFormInputHandler = (e) => {
+    const {name, value} = e.target;
+    setLoginDetails({...loginDetails, [name]: value})
+  }
+
+  const loginFormSubmitHandler = (e) => {
+    e.preventDefault();
+    loginHandler(loginDetails)
+  }
   return (
-    <div>Login</div>
+    <div>
+      <div>
+      {/* logo image here  */}
+      </div>
+
+      <h2>Login</h2>
+      <p>Join the Knot, Unlock New Connections...</p>
+
+      <form onSubmit={loginFormSubmitHandler}>
+        <input 
+          type="text" 
+          name="username"
+          value={loginDetails.username}
+          placeholder='Enter Username.'
+          onChange={loginFormInputHandler}
+        />
+
+        <div>
+          <input 
+            type={loginDetails.showPwd ? "text" : "password"}
+            name='password'
+            value={loginDetails.password}
+            placeholder='Enter Password.'
+            onChange={loginFormInputHandler}
+          />
+          {
+              !loginDetails.showPwd ? (
+                <AiOutlineEyeInvisible
+                  onClick={() => setLoginDetails({
+                    ...loginDetails, show : {...loginDetails, showPwd: !loginDetails.showPwd}
+                  })}
+                />
+              ) : (
+                <AiOutlineEye 
+                  onClick={() => setLoginDetails({
+                    ...loginDetails, show : {...loginDetails, showPwd: !loginDetails.showPwd}
+                  })}
+                />
+              )
+          }
+        </div>
+
+        <PrimaryButton type='submit'>
+          Login
+        </PrimaryButton>
+        <SecondaryButton
+          type='submit'
+          onClick={() => setLoginDetails({
+            ...loginDetails,
+            username : guestUserLoginDetails.username,
+            password : guestUserLoginDetails.password
+          })}
+        >
+          Login As A Guest
+        </SecondaryButton>
+      </form>
+      <p>
+        New To Knot Media?
+        <span onClick={() => navigate("/auth/signup")}>SignUp</span>
+      </p>
+    </div>
   )
 }
 
--- src/pages/Authentication/forms/Signup.jsx
+++ src/pages/Authentication/forms/Signup.jsx
@@ -1,8 +1,149 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../../contexts/authContext';
+import { PrimaryButton } from '../../../components/Button/Button';
 
 function Signup() {
+  const navigate = useNavigate();
+  const { signupHandler } = useAuth();
+
+  const [signupDetails,setSignupDetails] = useState*{
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    pwdMatch: true,
+    show: { pwd: false, confirmPwd: false },
+  }
+
+  document.title = "Signup | Knot"
+
+  const signupFormInputHandler = (e) => {
+    const {name, value} = e.target;
+    if(name === 'confirmPassword') {
+      setSignupDetails({
+        ...signupDetails,
+        [name] : value,
+        pwdMatch : value === signupDetails.password ? true : false
+      })
+    } else if (name === 'password') {
+      setSignupDetails({
+        ...signupDetails,
+        [name] : value,
+        pwdMatch: value === signupDetails.confirmPassword ? true : false
+      })
+    } else {
+      setSignupDetails({ ...signupDetails, [name]: value });
+    }
+  }
+
+  const signupFormSubmitHandler = (e) => {
+    e.preventDefault();
+    signupHandler(signupDetails)
+  }
   return (
-    <div>Signup</div>
+    <div>
+      <div>
+        {/* logo image here */}
+      </div>
+
+      <h2>SignUp</h2>
+      <p>Join the Knot, Unlock New Connections...</p>
+
+      <form onSubmit={signupFormSubmitHandler}>
+        <div>
+          <input 
+            type="text"
+            name="firstName" 
+            value={signupDetails.firstName}
+            placeholder="Enter First Name."
+            onChange={signupFormInputHandler}
+            required
+          />
+
+          <input
+            type="text"
+            name="lastName"
+            value={signupDetails.lastName}
+            placeholder="Enter Last Name."
+            onChange={signupFormInputHandler}
+            required
+          />
+
+          <input
+            type="text"
+            name="username"
+            value={signupDetails.username}
+            placeholder="Enter Username."
+            onChange={signupFormInputHandler}
+            required
+          />
+
+          <div>
+            <input 
+              type={signupDetails.show.pwd ? "text" : "password"}
+              name="password"
+              value={signupDetails.password}
+              placeholder="Enter Password."
+              onChange={signupFormInputHandler}
+              required
+            />
+            {
+              !signupDetails.show.pwd ? (
+                <AiOutlineEyeInvisible
+                  onClick={() => setSignupDetails({
+                    ...signupDetails, show : {...signupDetails.show, pwd: !signupDetails.show.pwd}
+                  })}
+                />
+              ) : (
+                <AiOutlineEye 
+                  onClick={() => setSignupDetails({
+                    ...signupDetails, show : {...signupDetails.show, pwd: !signupDetails.show.pwd}
+                  })}
+                />
+              )
+            }
+          </div>
+
+          <div>
+            <input
+              type={signupDetails.show.confirmPwd ? "text" : "password"}
+              name="confirmPassword"
+              value={signupDetails.confirmPassword}
+              placeholder="Confirm Password"
+              onChange={signupFormInputHandler}
+              required
+            />
+                        {
+              !signupDetails.show.pwd ? (
+                <AiOutlineEyeInvisible
+                  onClick={() => setSignupDetails({
+                    ...signupDetails, show : {...signupDetails.show, confirmPwd: !signupDetails.show.confirmPwd}
+                  })}
+                />
+              ) : (
+                <AiOutlineEye 
+                  onClick={() => setSignupDetails({
+                    ...signupDetails, show : {...signupDetails.show, confirmPwd: !signupDetails.show.confirmPwd}
+                  })}
+                />
+              )
+            }
+          </div>
+
+          <PrimaryButton
+            type='submit'
+            disabled={!signupDetails.pwdMatch}
+          >
+            SignUp
+          </PrimaryButton>
+        </div>
+      </form>
+      <p>Already have an account?
+      <span onClick={() => navigate("/auth/login")}>Login</span>
+      </p>
+    </div>
   )
 }
 
