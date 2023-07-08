795ff8bfb4fdcfd021b555aae43dda918b4d63c5
abhishek17551 <ababhishek113@gmail.com>
2023-07-08 14:10:49 +0530

    fix:fixed a faulty component,click function & added container styles

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -18,10 +18,10 @@ const Sidebar = () => {
         transform: "scale(1.25)",
     }
     return(
-        <aside>
-            <ul>
-                <li>
-                    <Link to="/">
+        <aside className="flex w-full items-center bg-[white] fixed bottom-0 left-0 sm:sticky sm:flex-col sm:justify-between sm:h-screen sm:top-0 sm:overflow-y-auto sm:border-0 sm:z-0 overflow-x-hidden  border-t-2 border-primaryDark dark:border-primaryLight dark:text-primaryLight dark:bg-darkBlack z-40">
+            <ul className="flex justify-around items-center px-2 py-1 tracking-wide grow sm:items-start  sm:justify-start  sm:py-4 sm:flex-col gap-3 sm:gap-2 ">
+                <li className="hidden sm:pb-2 sm:px-1 sm:block">
+                    <Link to="/" className="flex items-center">
                         {/* logo image here */}
                         <span>Knot-Media</span>
                     </Link>
@@ -67,27 +67,27 @@ const Sidebar = () => {
                     </NavLink>
                 </li>
 
-                <li onClick={() => setShowPostModal(true)}>
+                <li onClick={() => showPostModal(true)} className="cursor-pointer w-max px-0 sm:flex sm:px-1 lg:p-0 lg:w-full">
                     <HiPlusCircle/>
-                </li>
-
-                <li onClick={() => setShowSettingsModal(true)}>
-                    <UserAvatar user={currentUser}/>
                     <PrimaryButton>
                         <HiPlusCircle/>
                         <span>New Post</span>
                     </PrimaryButton>
                 </li>
+
+                <li onClick={() => setShowSettingsModal(true)} className="flex p-2 w-max sm:hidden" >
+                    <UserAvatar  user={currentUser} />
+                </li>
             </ul>
 
-            <ul>
-                <li onClick={() => setShowSettingsModal(true)}>
+            <ul className="hidden pr-2 sm:flex ">
+                <li onClick={() => setShowSettingsModal(true)} className="flex items-center justify-center gap-2  cursor-pointer p-3 w-max ">
                     <UserAvatar user={currentUser}/>
-                    <div>
+                    <div className="hidden text-sm lg:inline">
                         <p>{currentUser?.firstName + " " + currentUser?.lastName}</p>
                         <p>@{currentUser?.username}</p>
                     </div>
-                    <HiDotsHorizontal title="Settings Open"/>
+                    <HiDotsHorizontal title="Settings Options"/>
                 </li>
             </ul>
 
