18d9a3adef5c33289b701a423130074addb3da15
abhishek17551 <ababhishek113@gmail.com>
2023-07-17 19:50:29 +0530

    fix:added count of followers, included styles

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -30,11 +30,13 @@ const ProfileDetails = ({user}) => {
                 <img
                     src={user?.backgroundImage ? user?.backgroundImage : defaultBackgroundImage}
                     alt={user?.username ? user?.username+"background_image" : "Knot_Background_Image"}
+                    className="h-[12.5rem] w-full object-cover"
                 />
                 <div className="flex flex-col gap-2 w-full px-4 py-2">
                     <div className="flex justify-between w-full">
                         <UserAvatar
                             user={user}
+                            className="absolute h-[8rem] w-[8rem] top-[6.25rem]"
                         />
                         <div className="flex justify-center items-center gap-4 mb-2">
                             {
@@ -42,11 +44,13 @@ const ProfileDetails = ({user}) => {
                                     <>
                                         <SecondaryButton
                                             onClick={()=>setEditUserModal(true)}
+                                            className="py-1 px-3 rounded"
                                         >
                                             Edit Profile
                                         </SecondaryButton>
                                         <SecondaryButton
                                             onClick={logoutHandler}
+                                            className="py-1 px-3 rounded hover:bg-primaryRed"
                                         >
                                             Logout
                                         </SecondaryButton>
@@ -58,6 +62,7 @@ const ProfileDetails = ({user}) => {
                                                 ? handleButtonsClick(500,unfollowUserHandler,user?._id)
                                                 : handleButtonsClick(500,followUserHandler,user?._id)
                                             }
+                                            className="py-1 px-3 rounded"
                                         >
                                             {userAlreadyFollowing ? "Unfollow" : "Follow"}
                                         </SecondaryButton>
@@ -68,24 +73,26 @@ const ProfileDetails = ({user}) => {
                     </div>
 
                     <div>
-                        <p>
+                        <p className="text-xl font-bold">
                             {user?.firstName + " " + user?.lastName}
                         </p>
-                        <span>@{user?.username}</span>
+                        <span className="text-[grey] p-0">@{user?.username}</span>
                     </div>
                     {user?.bio && <span>{user?.bio}</span>}
                     {user?.website && (
                         <div className="flex gap-2 items-center">
-                            <HiLink/>
+                            <HiLink className="text-lg"/>
                             <Link to={user?.website} target="_blank">
-                                <span>{user?.website?.split("/")[2]}</span>
+                                <span className="text-sm text-[blue] cursor-pointer hover:underline dark:text-[skyblue]">
+                                    {user?.website?.split("/")[2]}
+                                </span>
                             </Link>
                         </div>
                     )}
 
                 <div className="flex gap-2 items-center">
                     <MdDateRange/>
-                    <span>{getPostDate(user?.createdAt)}</span>
+                    <span className="text-sm">{getPostDate(user?.createdAt)}</span>
                 </div>
 
                 <div className="flex items-center gap-4">
@@ -95,6 +102,7 @@ const ProfileDetails = ({user}) => {
                             title: "Following",
                             list: user?.following
                         }))}
+                        className="cursor-pointer hover:underline"
                     >
                         <span>Following</span>
                         <span
@@ -103,7 +111,9 @@ const ProfileDetails = ({user}) => {
                                 title: "Followers",
                                 list: user?.followers
                             }))}
+                            className="cursor-pointer hover:underline"
                         >
+                            <span className="font-bold">{user?.followers?.length}</span>
                             Followers
                         </span>
                     </span>
