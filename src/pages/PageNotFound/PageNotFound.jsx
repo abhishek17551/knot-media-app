8a83b8362d00c8369e4442eeb3bf829232d37f16
abhishek17551 <ababhishek113@gmail.com>
2023-08-14 20:02:20 +0530

    feat:created UserProfile,PageNotFound pages

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { errorImage } from '../../utils/constants';
+import { FaAngleDoubleLeft } from '../../utils/icons'
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
+  document.title = "Page Not Found"
   return (
-    <div>PageNotFound</div>
+    <div className='flex flex-col justify-center items-center gap-10 h-[110vh] -mt-10 dark:text-primaryLight'>
+      <img
+        src={errorImage}
+        alt='404ErrorImage'
+        className="w-full max-w-[30rem] px-2 "
+      />
+
+      <div onClick={() => navigate("/")} className='flex  items-center justify-center gap-2 -mt-6 py-2 px-4 cursor-pointer hover:scale-110 border rounded-md'>
+        <FaAngleDoubleLeft/>
+        <span>Back To Home</span>
+      </div>
+    </div>
   )
 }
 
